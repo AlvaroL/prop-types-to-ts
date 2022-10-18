062f09befc01180a86f3f3423b122267921e5e71
AlvaroL <alvaro.llorente.ramos@gmail.com>
2022-10-18 23:05:55 +0200

    handle multiple files and some exceptions

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import {Parser} from "acorn";
 import jsx from "acorn-jsx";
 import fs from 'fs';
+import glob from 'glob';
 import path from 'path';
+
 import {fileURLToPath} from 'url';
 
 import type {Node} from 'acorn';
@@ -9,19 +11,16 @@ import type {Node} from 'acorn';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const JSXParser = Parser.extend(jsx());
 
-const FILE_NAME = `${__dirname}/tests/Person.jsx`;
-const content = fs.readFileSync(FILE_NAME).toString('utf-8');
-
-const ast = JSXParser.parse(content, {ecmaVersion: 'latest', sourceType: 'module'});
-const expressions: Array<Node> = ast.body.filter(({type}: Node) => type === 'ExpressionStatement');
-
-const fileContent: Record<string, string> = {};
+const fileContent: Record<string, string> = {
+    react: `import { FC } from "react";`
+};
 
 const enum PropertyType {
     BOOLEAN = 'boolean',
     NUMBER = 'number',
     STRING = 'string',
     FUNCTION = '() => void',
+    ANY = 'any',
 }
 
 const propToType: Record<string, PropertyType> = {
@@ -29,9 +28,15 @@ const propToType: Record<string, PropertyType> = {
     number: PropertyType.NUMBER,
     string: PropertyType.STRING,
     func: PropertyType.FUNCTION,
+    any: PropertyType.ANY,
 }
 
-const getTypeForOneOf = (property = '', values = []): string => {
+const getPropType = (str: string): PropertyType => {
+    const propType = propToType[str];
+    return propType ? propType : PropertyType.ANY;
+}
+
+const getTypeForOneOf = (property = '', values: Array<{value: string}> = []): string => {
     const typeName = property[0].toUpperCase() + property.slice(1);
     fileContent[property] = `export type ${typeName} = ${values.map(({value}) => `"${value}"`).join(' | ')};`;
     return typeName;
@@ -43,52 +48,87 @@ interface Property {
     mandatory: boolean;
 }
 
+const isOneOf = (node: Node): boolean => node.type === 'CallExpression' && node.callee.property.name === 'oneOf';
 
-const getTypeForNode = (property = '', node: Node): Property => {
-    if (node.type === 'CallExpression' && node.callee.property.name === 'oneOf') {
-        return {
-            name: property,
-            mandatory: false,
-            type: getTypeForOneOf(property, node.arguments[0].elements),
-        }
-    } else {
-        if (node.property.name === 'isRequired') {
+const getTypeForNode = (prop: Node): Property => {
+    if (prop.type === 'SpreadElement') {
+        return { name: '', type: PropertyType.ANY, mandatory: false };
+    }
+    const property = prop.key.name;
+    const node = prop.value;
+    try {
+        if (isOneOf(node)) {
             return {
                 name: property,
-                type: node.object.type === 'CallExpression'
-                    ? getTypeForOneOf(property, node.object.arguments[0].elements)
-                    : propToType[node.object.property.name],
-                mandatory: true,
+                mandatory: false,
+                type: getTypeForOneOf(property, node.arguments[0].elements),
             }
+        } else {
+            if (node.property.name === 'isRequired') {
+                return {
+                    name: property,
+                    type: node.object.type === 'CallExpression'
+                        ? getTypeForOneOf(property, node.object.arguments[0].elements)
+                        : getPropType(node.object.property.name),
+                    mandatory: true,
+                }
+            }
+            return {
+                name: property,
+                type: getPropType(node.property.name),
+                mandatory: false,
+            };
         }
+    } catch(e) {
+        console.error(`ERROR getTypeForNode => ${property}`, e);
         return {
             name: property,
-            type: propToType[node.property.name],
+            type: PropertyType.ANY,
             mandatory: false,
-        };
+        }
     }
+
 }
 
 const propertyToString = ({name, mandatory, type}: Property): string => {
-    return `${name}${mandatory ? '' : '?'}: ${type}`;
+    return name ? `${name}${mandatory ? '' : '?'}: ${type}` : '';
 }
 
-const buildInterface = (interfaceName: string, properties: Array<Node>) => {
-  const newInterface = `
-export interface ${interfaceName} {
-  ${properties.map((prop) => `${propertyToString(getTypeForNode(prop.key.name, prop.value))}`).join(';\n  ')}
-}`
-    fileContent[interfaceName] = newInterface;
+const buildInterface = (interfaceName: string, properties: Array<Node>): string => {
+    const propsInterfaceName = `${interfaceName}Props`;
+    return `
+export interface ${propsInterfaceName} {
+  ${properties.map((prop) => propertyToString(getTypeForNode(prop)))
+    .filter(e => !!e)
+    .join('\n  ')}
+}
+
+declare const ${interfaceName}: FC<${propsInterfaceName}>`;
 };
 
-expressions.forEach(({expression}) => {
-    const {left: {object: {name: className}, property}, right} = expression;
-    if (property.name === 'propTypes') {
-        buildInterface(className, right.properties);
-        const fileName = FILE_NAME.split('/').pop();
-        const tsFileName = fileName.replace('.jsx', '.ts');
-        fs.writeFileSync(`${__dirname}/../generated/${tsFileName}`, Object.values(fileContent).join('\n\n'));
+const dir = process.argv.find(e => e.indexOf('dir') !== -1)?.split('=').pop();
+const out = process.argv.find(e => e.indexOf('out') !== -1)?.split('=').pop();
+
+glob('**/*.jsx', { cwd: dir,}, (err: Error | null, matches: string[]) => {
+    if (err) {
+        console.error(err);
+        return;
     }
+    matches.filter((v) => v.indexOf('__tests__') === -1 )
+        .forEach(fileName => generateDeclaration(`${dir}/${fileName}`));
+    fs.writeFileSync(`${__dirname}/../generated/index.d.ts`, Object.values(fileContent).join('\n'));
 });
 
-console.log('Done writing file');
+const generateDeclaration = (filePath: string) => {
+    console.log('generating file ', filePath)
+    const content = fs.readFileSync(filePath).toString('utf-8');
+    const ast = JSXParser.parse(content, {ecmaVersion: 'latest', sourceType: 'module'});
+    const expressions: Array<Node> = ast.body.filter(({type}: Node) => type === 'ExpressionStatement');
+
+    expressions.forEach(({expression}) => {
+        const {left: {object: {name: className}, property}, right} = expression;
+        if (property.name === 'propTypes') {
+            fileContent[className] = buildInterface(className, right.properties);
+        }
+    });
+}
